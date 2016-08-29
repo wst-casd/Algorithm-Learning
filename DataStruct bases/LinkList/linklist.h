de0cb5460d08b837284421b3d5428ff8681dfc10
wst-casd <stwang.casd@gmail.com>
2016-08-29 20:39:20 +0800

    update linklist.h

--- DataStruct bases/LinkList/linklist.h
+++ DataStruct bases/LinkList/linklist.h
@@ -1,5 +1,15 @@
+#ifndef _LINKLIST_H
+#define _LINKLIST_H
 
 typedef struct  NODE{
     int value;
     struct NODE *next;
 } Node;
+
+Node* createList(  int num );
+int listLength( Node *head );
+int insert( Node **head, const int value );
+int delete( Node **head, Node *node );
+void reverse( Node **head );
+
+#endif
